ecf913713e7ab30d676eacf6329a5656e22b4180
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-01-10 15:11:42 +0545

    Added p15 for finding GCD using recursion method

---
+++ p15.c
@@ -0,0 +1,21 @@
+/*
+C program to find GCD of given number using recursion
+*/
+#include <stdio.h>
+int FindGCD(int, int);
+int main(void)
+{
+    int x, y, z;
+    printf("Enter two numbers to find their GCD: ");
+    scanf("%d%d", &x, &y);
+    z = FindGCD(x, y);
+    printf("The GCD for %d and %d is %d.\n", x, y, z);
+    return 0;
+}
+int FindGCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return FindGCD(b, a % b);
+}
