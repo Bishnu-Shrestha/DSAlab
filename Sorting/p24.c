ea7eb77e349ec7f612ff4da5de1312a393084c92
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-02-11 17:14:51 +0545

    Added merge function for merge sort

--- Sorting/p24.c
+++ Sorting/p24.c
@@ -2,8 +2,8 @@
 C program to perform merge sort
 */
 #include <stdio.h>
-void merge_sort(int *, int, int);
-void merge(int *, int, int, int);
+void merge_sort(int *, int);
+void merge(int *, int *, int *, int);
 int main()
 {
     int size;
@@ -15,25 +15,67 @@ int main()
         printf("Enter the %d element: ", i);
         scanf("%d", &arr[i]);
     }
-    merge_sort(arr, 0, size);
-}
-// Function to divide the array into halves
-void merge_sort(int *a, int l, int r)
-{
-    if (l < r)
+    merge_sort(arr, size);
+    // Displaying the sorted array
+    printf("After sorting:\n");
+    for (int i = 0; i < size; i++)
     {
-        int m = (l + r) / 2;
-        merge_sort(a, l, m);
-        merge_sort(a, m + 1, r);
-        merge(a, l, m + 1, r);
+        printf("%d\t", arr[i]);
     }
 }
-void merge(int *a, int l, int m, int r){
-int x=l,y=m,z=r;
-int b[z],a[y];
-while (x<m&&y<=r)
+// Function to divide the array into halves
+void merge_sort(int *a, int r)
 {
-  
+    if (r <= 1) // base case
+        return;
+    // size for the left and right portion of the array
+    int m = r / 2, n = r - m;
+    int leftArr[m], rightArr[r - m];
+    int i = 0, j = 0; // index trackers
+    for (; i < r; i++)
+    {
+        if (i < m)
+            leftArr[i] = a[i];
+        else
+        {
+            rightArr[j] = a[i];
+            j++;
+        }
+    }
+    merge_sort(leftArr, m);
+    merge_sort(rightArr, n);
+    merge(leftArr, rightArr, a, r);
 }
+// Function to merge the left half and the right half of the initial array
+void merge(int *lAr, int *rAr, int *arr, int size)
+{
 
+    int leftSiz = size / 2, rightSiz = size - leftSiz;
+    int i = 0, l = 0, r = 0;
+    while (l < leftSiz && r < rightSiz)
+    {
+        if (lAr[l] < rAr[r])
+        {
+            arr[i] = lAr[l];
+            l++;
+        }
+        else
+        {
+            arr[i] = rAr[r];
+            r++;
+        }
+        i++;
+    }
+    while (l < leftSiz)
+    {
+        arr[i] = lAr[l];
+        i++;
+        l++;
+    }
+    while (r < rightSiz)
+    {
+        arr[i] = rAr[r];
+        i++;
+        r++;
+    }
 }
