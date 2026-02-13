20f252a966680df9802493880e73b4262023ebe3
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-02-13 19:55:09 +0545

    Added main function for heap sort

--- Sorting/p26.c
+++ Sorting/p26.c
@@ -2,7 +2,35 @@
 program to perform heap sort
 */
 #include <stdio.h>
+// structure for node of the heap
+typedef struct node
+{
+    int data;
+    struct node *left, *right;
+} Node;
+
+void heapify(Node *);
+void heapsort(int *);
+
 int main()
 {
+    int size;
+    printf("Enter teh size of data to sort: ");
+    scanf("%d", &size);
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter the %d element: ", i);
+        scanf("%d", &arr[i]);
+    }
+    heapsort(arr);
+    // Displaying the sorted array
+    printf("After sorting:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
     return 0;
 }
+void heapify(Node *);
+void heapsort(int *);
