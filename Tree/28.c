a8c3ba6c7a812c14f930d953e9a7a3099659aa16
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-02-24 21:44:23 +0545

    Added insert function for heap

--- Tree/28.c
+++ Tree/28.c
@@ -1,9 +1,55 @@
 /*
-program8 for heap implementation
+program for heap data structure implementation
 */
-#inclide<stdio.h>
-ont main()
+#include <stdio.h>
+#define max 10
+void Insert(int *, int);
+void Delete(int *, int);
+void Display(int *, int);
+void Heapify(int *, int, int);
+int main()
 {
-  printf("Heap program to code");
-  return 0;
+    int arr[max], size, ch;
+    printf("Enter the size of heap(<=%d): ", max);
+    scanf("%d", &size);
+    while (ch != 5)
+    {
+        printf("\n****** MENU ******\n1. Insert\n2. Delete\n3. Display\n4. Heapify\n5. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            Insert(arr, size);
+            break;
+        case 2:
+            Delete(arr, size);
+            break;
+        case 3:
+            Display(arr, size);
+            break;
+        case 4:
+            Heapify(arr, size, 0);
+            break;
+        case 5:
+            printf("Exiting program.\n");
+            break;
+        default:
+            printf("Invalid choice.\nTry again.\n");
+        }
+    }
+    return 0;
+}
+void Insert(int *a, int n)
+{
+    if (n >= max)
+    {
+        printf("Heap is full. Cannot insert new element.\n");
+        return;
+    }
+    int key;
+    printf("Enter the element to insert: ");
+    scanf("%d", &key);
+    a[n] = key;
+    Heapify(a, n, 0);
 }
