fae3b69423d2026785b7f3582e7bf2a4744eb7f4
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-03-06 19:39:01 +0545

    added file for the binary tree operations

---
+++ Tree/p29.c
@@ -0,0 +1,7 @@
+// cprogram to create a binary tree and perform inorder, preorder and postorder traversals
+#include <stdio.h>
+int main()
+{
+    printf("This program is for binary tree implementation.\n");
+    return 0;
+}
