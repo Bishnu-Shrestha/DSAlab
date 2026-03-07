6cb1c53ecb780a9b18dc38a602c5490fa6be7f5b
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-03-07 19:28:19 +0545

    Added tree input

--- Tree/p29.c
+++ Tree/p29.c
@@ -1,7 +1,18 @@
 // cprogram to create a binary tree and perform inorder, preorder and postorder traversals
 #include <stdio.h>
+#define max 100
 int main()
 {
+    int size;
+    int arr[max];
     printf("This program is for binary tree implementation.\n");
+    printf("Enter the size of the tree: ");
+    scanf("%d", &size);
+    printf("Enter the elements of the tree: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
     return 0;
 }
