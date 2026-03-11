66262860585527c55a39acc34932b9f5b2e7569f
Bishnu Shrestha <bishnu011shrestha@gmail.com>
2026-03-11 16:54:46 +0545

    added menu and insert function|

--- Tree/p29.c
+++ Tree/p29.c
@@ -8,9 +8,58 @@ typedef struct TreeNode
     struct TreeNode *left;
     struct TreeNode *right;
 }tree;
+tree *root=NULL;
+tree* createNode(int data)
+{
+    tree *newNode = (tree*)malloc(sizeof(tree));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}   
 int main()
 {
     printf("This program is for binary tree implementation.\n");
-  
+        tree *root = NULL;
+        int choice, value;
+        while (1)
+        {
+            printf("1. Insert\n2. Inorder Traversal\n3. Preorder Traversal\n4. Postorder Traversal\n5. Exit\n");
+            printf("Enter your choice: ");
+            scanf("%d", &choice);
+            switch (choice)
+            {
+                case 1:
+                    printf("Enter the value to insert: ");
+                    scanf("%d", &value);
+                    createNode(value);
+                    break;
+                case 2:
+
+                    // Call the inorder traversal function here
+                    break;
+                case 3:
+                    // Call the preorder traversal function here
+                    break;
+                case 4:
+                    // Call the postorder traversal function here
+                    break;
+                case 5:
+                    exit(0);
+                default:
+                    printf("Invalid choice! Please try again.\n");
+            }
+        }
     return 0;
+}
+tree* insertNode(tree* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
 }
